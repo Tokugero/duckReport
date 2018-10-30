8b6e317bc4ffdb129eb885dd3bb5b4e5d4a5f051
Phil Almueti <palmueti@gmail.com>
2018-10-30 12:02:09 -0700

    Adding some simple comments

--- functions/duckduckgo.py
+++ functions/duckduckgo.py
@@ -6,22 +6,26 @@ import urllib
 
 #https://api.duckduckgo.com/?q=valley+forge+national+park&format=json&pretty=1
 
+#Define the search function
 def query(search):
 	url = "https://duckduckgo.com/html/?q="
 	request = requests.get(url+search)
 	results = beautify(request.text)
 	return results
 
+#Clean up the search results, they'll all be gobbledy html
 def beautify(results):
 	resultsList = []
 	resultsPretty = BeautifulSoup(results)
 	topics = resultsPretty.findAll('div',{'id':'links'})[0]
 	answers = topics.findAll('div',{'class': re.compile('results_*')})
 	for answer in answers:
+		#TODO: write a proper class for this to share on all the search engines
 		resultObj = {"url":urllib.unquote(answer.a['href'][15:]), "answer":answer.text[24:]}
 		resultsList.append(resultObj)
 	return resultsList
 	
 
+#A simple test call if ran directly
 if __name__=="__main__":
 	query("test")
--- search.py
+++ search.py
@@ -4,16 +4,27 @@ import json
 from functions import duckduckgo
 import re
 
-searchQuery = "free clinic massachusetts inurl:contact -animal"
+#This is a hardcoded query that will be a user input, the inurl & query tuning should be easier options to find or implicitly added
+rawSearchQuery = "free clinic massachusetts inurl:contact -animal"
 
-ddgResults = duckduckgo.query(searchQuery.replace(" ","+"))
+#transform the query into a usable string
+searchQuery = rawSearchQuery.replace(" ","+")
 
+#pass queries to search engines
+ddgResults = duckduckgo.query(searchQuery)
+
+
+#Transform queries here
+
+#This is to transform ddg results from the form of [{url:"url",answer:"answer"}]
 for result in ddgResults:
 	phoneNum = "No number found, search page directly"
+	#parse out phone numbers
 	rpattern = "(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
 	phoneNumParse = re.search(rpattern, result["answer"])
 	if phoneNumParse:
 		phoneNum = phoneNumParse.group(1)
+	#This outputs to the console, but it should respond with something more useful
 	print result["url"]+"\n"+result["answer"]+"\n"+phoneNum+"\n-------------\n"
 
 print len(ddgResults)
